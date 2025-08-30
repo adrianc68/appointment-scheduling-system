31461e95bad088b1135164b1d8635ed059bb6f84
adrianc68 <angeladriancamalgarcia@hotmail.com>
2025-08-30 11:13:23 -0600

    fix: environment prod ts file

--- AppointmentSchedulerWeb/src/environments/environment.prod.ts
+++ AppointmentSchedulerWeb/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //apiBaseUrl: 'API_BASE_URL_PLACEHOLDER'
-  apiBaseUrl: 'http://192.168.50.63:5120'
+  apiBaseUrl: 'API_BASE_URL_PLACEHOLDER'
+  //apiBaseUrl: 'http://192.168.50.63:5120'
 
 };
 
